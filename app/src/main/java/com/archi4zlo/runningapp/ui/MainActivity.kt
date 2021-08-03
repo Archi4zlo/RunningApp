721e291863571f485ff41f3acfedb799ee9a432b
Anton Sych <anton.sych.job@gmail.com>
2021-08-03 11:42:21 +0300

    Added location permissions (EasyPermissions library) to RunFragment.kt Setting up Navigation (Main Navigation and bottom_nav_menu.xml) Added map lifecycle into TrackingFragment.kt (You need to change Google_api_key in string.xml to your API_KEY )

--- app/src/main/java/com/archi4zlo/runningapp/ui/MainActivity.kt
+++ app/src/main/java/com/archi4zlo/runningapp/ui/MainActivity.kt
@@ -3,22 +3,34 @@ package com.archi4zlo.runningapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.archi4zlo.runningapp.R
 import com.archi4zlo.runningapp.db.RunDao
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
-
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    @Inject
-    lateinit var runDao: RunDao
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("runDao","RUNDAO: ${runDao.hashCode()}")
+        setSupportActionBar(toolbar)
+
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                when(destination.id){
+                    R.id.settingsFragment, R.id.runFragment,R.id.statisticsFragment ->
+                        bottomNavigationView.visibility = View.VISIBLE
+
+                    else -> bottomNavigationView.visibility = View.GONE
+                }
+            }
     }
 }
